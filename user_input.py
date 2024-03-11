fb139df9c2e04c8e8de2386d03c121ce3a82788b
Purity mwinja <nyawirapurity94@gmail.com>
2024-03-11 21:58:06 +0300

    Making my first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name= input('Fill in your full names')
+age= input('what is your age?')
+location= input('what is your location')
+print("Hello, my name is", name, "and I am", age, "years old and i live in", location)
